db213a85228e253ad4cf1058ee45879b3fee1a70
Ponlawat Lueakaeo <76694369+h2ofriday@users.noreply.github.com>
2024-03-23 22:23:43 +0700

    fix : asd

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -35,13 +35,13 @@ const Navbar: React.FC<NavbarProps> = () => {
   }
 
   return (
-    <header className='bg-white'>
+    <header className='bg-white fixed w-full z-50'>
       <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8' aria-label='Global'>
         <div className='flex lg:flex-1'>
-          <a href='/' className='-m-1.5 p-1.5'>
+          <Link href='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Your Company</span>
             <img className='h-8 w-auto' src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600' alt='' />
-          </a>
+          </Link>
         </div>
         <div className='flex lg:hidden'>
           <button
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,20 +2,16 @@ import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-
 interface LayoutProps {
   children?: React.ReactNode
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-
-
   return (
     <React.Fragment>
       <div className="w-screen">
-        
-      <Navbar />
-        <div style={{paddingTop: '10px'}} className="min-h-[calc(100vh-70px-88px)]">{children}</div>
+        <Navbar />
+        <div style={{paddingTop: '100px'}} className="min-h-[calc(100vh-70px-88px)] pt-12">{children}</div>
         <Footer />
       </div>
     </React.Fragment>
